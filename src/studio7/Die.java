8b8a6f3cc86ac231fbd6c98d21dca80c1a3ea0be
aperc <aperc@LAPTOP-1OC4R27L.dhcp.wustl.edu>
2022-11-03 14:14:23 -0500

    done

--- src/studio7/Die.java
+++ src/studio7/Die.java
@@ -1,5 +1,19 @@
 package studio7;
 
 public class Die {
-
+	private int sides;
+	
+	public Die(int sides) {
+		this.sides = sides;
+	}
+	
+	public int getSides() {
+		return this.sides;
+	}
+	public void setSides(int sides) {
+		this.sides = sides;
+	}
+	public int rollDie() {
+		return (int)((Math.random()*this.sides)+1);
+	}
 }
--- src/studio7/Rectangle.java
+++ src/studio7/Rectangle.java
@@ -2,6 +2,8 @@ package studio7;
 
 import java.awt.Color;
 
+import edu.princeton.cs.introcs.StdDraw;
+
 public class Rectangle {
 	private double length;
 	private double height;
@@ -25,8 +27,17 @@ public class Rectangle {
 		return this.length*this.height;
 		
 	}
-	
 	public double perimeter() {
 		return (this.length*2.0)+(this.height*2.0);
+	}
+	public boolean isSmaller(Rectangle x) {
+		return this.findArea() < x.findArea();
+	}
+	public boolean isSquare() {
+		return this.length == this.height;
+	}
+	public void draw(double x, double y) {
+		StdDraw.filledRectangle(x, y, this.length/2, this.height/2);
+		
 	}
 }
